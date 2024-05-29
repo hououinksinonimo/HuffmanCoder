35cfcb9d6e0f4df8222a2b804cd5edac889914b6
hououinksinonimo <125733245+hououinksinonimo@users.noreply.github.com>
2024-05-29 04:14:54 +0300

    Add files via upload

--- Decompress.h
+++ Decompress.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <string>
 
 void create_binary_tree_of_elementary_codes(uint8_t &length_of_last_byte, uint8_t &last_byte, Binary_tree &tree);
 void decompress_data_from_file(Tree_element* const root, const std::string &filename, uint8_t &length_of_last_byte, const uint8_t &last_byte);
